1c228732db983adcb2017d2e5440cb041cb78448
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2024-12-20 15:32:40 -0500

    implement initial navigation

---
+++ app/onboarding.tsx
@@ -0,0 +1,30 @@
+import { View, StyleSheet, Button } from "react-native";
+import { theme } from "@/theme";
+import { useRouter } from "expo-router";
+import { useUserStore } from "@/store/userStore";
+
+export default function OnboardingScreen() {
+  const router = useRouter();
+
+  const toggleHasOnboarded = useUserStore((state) => state.toggleHasOnboarded);
+
+  const handlePress = () => {
+    toggleHasOnboarded();
+    router.replace("/");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Let me in!" onPress={handlePress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colorWhite,
+  },
+});
